b61cc6d359a1dd536a709f927e14f3f581125e3d
kielstr <kielstr@cpan.org>
2012-11-21 19:28:26 +1100

    Clean for release

--- lib/Cron/RunJob.pm
+++ lib/Cron/RunJob.pm
@@ -18,7 +18,7 @@ our @ISA = qw(Exporter);
 our %EXPORT_TAGS = ( 'all' => [ qw() ] );
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw();
-our $VERSION = '0.00003';
+our $VERSION = '0.03';
 
 my ($job_pid, %_data);
 $SIG{TERM} = $SIG{INT} = sub {
