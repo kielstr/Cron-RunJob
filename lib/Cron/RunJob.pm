257ee734b5ca5c16045fa20a840482f4ec96a6fb
kielstirlin <kielstr@cpan.org>
2012-11-29 14:41:56 +1100

    fixed CPAN depend error

--- lib/Cron/RunJob.pm
+++ lib/Cron/RunJob.pm
@@ -18,7 +18,7 @@ our @ISA = qw(Exporter);
 our %EXPORT_TAGS = ( 'all' => [ qw() ] );
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw();
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 my ($job_pid, %_data);
 $SIG{TERM} = $SIG{INT} = sub {
